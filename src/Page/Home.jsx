ac70701637be319e604b9e992af7720afc15b7ef
amitbd599 <amitbd599@gmail.com>
2022-03-19 14:54:30 +0600

    first commit

---
+++ src/Page/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../Components/Universal_Components/Header';
+
+const Home = () => {
+    return (
+        <>
+            <Header></Header>
+        </>
+    );
+};
+
+export default Home;
